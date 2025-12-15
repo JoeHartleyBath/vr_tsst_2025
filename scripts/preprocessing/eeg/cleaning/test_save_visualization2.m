160c87c8b19be36d5f84aded4f9488aa24da149f
Joe Hartley <jh3968@bath.ac.uk>
2025-12-15 02:54:25 +0000

    Test: add test scripts for save_visualization function

---
+++ scripts/preprocessing/eeg/cleaning/test_save_visualization2.m
@@ -0,0 +1,7 @@
+addpath('../../../../scripts/preprocessing/eeg/cleaning');
+EEG = pop_importdata('dataformat','array','data',randn(128,1000));
+EEG.srate = 125;
+EEG.chanlocs = readlocs('../../../../config/chanlocs/NA-271.elc');
+folder = '../../../../output/vis/P99';
+filename = 'test_save.png';
+save_visualization(EEG, folder, filename);
